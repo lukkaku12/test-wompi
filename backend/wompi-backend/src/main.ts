1106ad78e8ffd66b6b94375f8b33cb3b7bd35cb7
Lucas <lucasaristiz789@gmail.com>
2026-01-11 22:17:06 -0500

    feat: add helmet middleware for enhanced security

--- backend/wompi-backend/src/main.ts
+++ backend/wompi-backend/src/main.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
